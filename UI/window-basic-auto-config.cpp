6640178c25df2015b7a38bfc9415f432d7e3dcfc
poccariswet <poccariswet2727@gmail.com>
2021-01-05 13:56:05 +0900

    rtmp-services: Add OPENREC.tv service

--- UI/window-basic-auto-config.cpp
+++ UI/window-basic-auto-config.cpp
@@ -629,6 +629,9 @@ void AutoConfigStreamPage::UpdateKeyLink()
 		streamKeyLink = "https://www.app.youstreamer.com/stream/";
 	} else if (serviceName == "Trovo") {
 		streamKeyLink = "https://studio.trovo.live/mychannel/stream";
+	} else if (serviceName.startsWith("OPENREC.tv")) {
+		streamKeyLink =
+			"https://www.openrec.tv/login?keep_login=true&url=https://www.openrec.tv/dashboard/live?from=obs";
 	}
 
 	if (QString(streamKeyLink).isNull()) {
